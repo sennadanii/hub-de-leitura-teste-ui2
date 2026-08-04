7a6c3b00ef4a1184a07c563606fcc3518470cd35
Danielle Senna <sennadanii@live.com>
2026-08-04 20:50:25 -0300

    correcoes no jenkinsfile

--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -26,7 +26,7 @@ describe('Funcionalidade: Busca no Catálogo', () => {
         })
     });
 
-    it.only('Deve validar todos os livros da lista', () => {
+    it('Deve validar todos os livros da lista', () => {
         cy.fixture('livros').then((cat) => {
             cat.forEach(item=>{
             cy.get('#search-input').clear().type(item.livro)
